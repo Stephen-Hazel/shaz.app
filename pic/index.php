522940d0b96326d3d2913c82040e8e67e116eeab
Stephen Hazel <sh@shaz.app>
2025-09-25 07:39:57 -0700

    ok pic is pretty solid w proper thumbnail size (pl) and comments

--- pic/index.php
+++ pic/index.php
@@ -1,142 +1,175 @@
 <? # pic/index.php - show some dang pics
+   # 2 dir levels - year, picset, then buncha .jpgs
+   # go script makes idx/yyyy/picset/picset.txt indexes with
+   # P/L|filename|comment      ( row 0 has extra |picset comment )
+   # and thumbnails in year/picset dir with same filename as main pic
 
 require_once ("../_inc/app.php");
 
 #dump('index',$_REQUEST);
-   $f = arg ('f', '');
-   $s = arg ('s', '');
-#dbg("f=$f s=$s");
+   $y = arg ('y', '');                 # year pos arg
+   $s = arg ('s', '');                 # picset pos arg
+#dbg("y=$y s=$s");
 
-   $Fold = LstDir ("pic", 'd');   sort ($Fold);
-   if ($f == '')  $fPos = count ($Fold)-1;
-   else           $fPos = $f;
-   $fStr = $Fold [$fPos];
-#dump("fPos=$fPos fStr=$fStr Fold:", $Fold);
+   $Year = LstDir ("pic",       'd');   sort ($Year);
+## if no y arg use last year we got
+   if ($y == '')    $yPos = count ($Year)-1;
+   else             $yPos = $y;
+   $yStr = $Year [$yPos];
 
-   $PSet = LstDir ("pic/$fStr", 'd');   sort ($PSet);
+   $PSet = LstDir ("pic/$yStr", 'd');   sort ($PSet);
+## got year but no picset, use 1st ps of year else 1st
    if ($s == '') {
-      if ($f=='') $sPos = count ($PSet)-1;
-      else        $sPos = 0;
+      if ($y == '') $sPos = count ($PSet)-1;
+      else          $sPos = 0;
    }
-   else           $sPos = $s;
+   else             $sPos = $s;
    $sStr = $PSet [$sPos];
+#dump("yPos=$yPos yStr=$yStr Year:", $Year);
 #dump("sPos=$sPos sStr=$sStr PSet:", $PSet);
 
-   $Pic = explode ("\n", Get ("idx/$fStr/$sStr.txt"));
-#dump("pic:", $Pic);
+   $Pic = explode ("\n", Get ("idx/$yStr/$sStr.txt"));
+   array_pop ($Pic);                   # kill last empty row from last \n
+#dump("pic:", $Pic);                   # cuz list of line\n leaves extra \n
+
+## page comment
+   $r0 = explode ('|', $Pic [0]);   $pCom = $r0 [3];
 
    pg_head ("pic", "jqui app", "jqui app");
 ?>
  <meta property="og:type"  content="website">
  <meta property="og:url"   content="https://shaz.app/pic/index.php<?=
-                                   "?f=$f&s=$s" ?>">
- <meta property="og:title" content="Stevez pics <?= $fStr.' '.$sStr ?>">
+                                                               "?y=$y&s=$s" ?>">
+ <meta property="og:title" content="Stevez pics <?= "$yStr $sStr $pCom" ?>">
  <meta property="og:image" content="https://shaz.app/pic/idx/<?=
-                                   "$fStr/$sStr/".explode ('|',$Pic[0])[1] ?>">
+                                   "$yStr/$sStr/".explode ('|',$Pic[0])[1] ?>">
  <style>
 .comment {
    max-width: 640px;
-   font-size: 22pt;
+   font-size: 18pt;
    color:            #003050;
    background-color: #00F0FF;
-   padding: 10px;
+   padding: 5px;
    border:  solid 3px #0F7391;
+}
+.thumb {
+   float:    left;
+   position: relative;
+}
+.thumbtxt {
+   position:   absolute;
+   top:        0;
+   left:       0;
+   font-size:  18pt;
+   color:      white;
+   background: rgb(0,0,0);
+   background: rgba(0,0,0,.5);
+   text-shadow: 2px 2px 4px rgba(0,0,0,0.7);
+/* color:            #003050;
+   background-color: #00F0FF; */
+}
+#big {
+/* position:            relative; */
+   display:             flex;
+   justify-content:     center;
+   align-items:         center;
+   background-size:     contain;
+   background-position: center;
+   background-repeat:   no-repeat;
+}
+#bigtxt {
+   position:   absolute;
+   z-index:    1;
+   top:        0;
+   left:       0;
+   color:      white;
+   background: rgb(0,0,0);
+   background: rgba(0,0,0,.5);
+   text-shadow: 2px 2px 4px rgba(0,0,0,0.7);
 }
  </style>
  <script>
-const path = "<?= "pic/$fStr/$sStr/" ?>";
-const pThm = "<?= "idx/$fStr/$sStr/" ?>";
-const pic  = <?= json_encode ($Pic) ?>;
-const parr = [];
-const pLen = pic.length;
-let   pPos = pLen;
+const path = "<?= "pic/$yStr/$sStr/" ?>";   // path for full pics
+const pic  =  <?= json_encode ($Pic) ?>;    // pic[][0..2] = L/P|filenm|comment
 
 function reArg (s)
-{ let f = $('#fold').prop ('selectedIndex')
-  let a = "index.php?f="+f
-   if (s > -1)  a += ("&s=" + s)
-   location.href = a
+{ let a = "index.php?y=" + $('#year').prop ('selectedIndex');
+   if (s > -1)  a += ("&s=" + s);
+   location.href = a;
 }
 
-function reFold ()  { reArg (-1); }
+function reYear ()  { reArg (-1); }
 function rePSet ()  { let s = $('#pset').prop ('selectedIndex');   reArg (s);  }
 function prevSet () { let s = $('#pset').prop ('selectedIndex');   reArg (s-1);}
 function nextSet () { let s = $('#pset').prop ('selectedIndex');   reArg (s+1);}
 
-function redo ()
-// thumbnails
-{ let h = ''
-   $('#info').html ('')
-   h += "<center>";
-// if (pic [0] != '')
-//    h += "<div class='comment'>" +  + "</div>";
-   for (let i = 0;  i < pLen;  i++) {
-     let a = pic [i].split('|');
-//   let pl = a[0];
-     let fn = a[1];
-     let cm = a[2];                 // thumbnail path here
-     let t = (cm != '') ? ('title="' + cm + '" ') : '';
-      h += "<img class='pic' onclick='big("+i+");' " + t +
-                'src="' + pThm + fn + '"' + ">\n";
-   }
-   h += "</center>";
-   $('#redo').html (h);
+function full (fn = '')
+// toggle fullscreen on/off
+{ const it = document.querySelector ('#big');
+   if (fn != '')  it.style.backgroundImage = fn;
+
+   if            (it.requestFullScreen)       it.requestFullscreen ();
+   else if (it.webkitRequestFullscreen) it.webkitRequestFullscreen ();
+   else if     (it.msRequestFullscreen)     it.msRequestFullscreen ();
+
+   if            (document.fullscreenElement)       document.exitFullscreen ();
+   else if (document.webkitFullscreenElement) document.webkitExitFullscreen ();
+   else if     (document.msFullscreenElement)     document.msExitFullscreen ();
 }
 
-function full ()
-{
-   document.querySelector ('#big').requestFullscreen ();
-   if (document.fullscreenElement)  document.exitFullscreen ();
-}
-
-function un ()  {$("#info").html ('');}
+function un ()  {$("#full").html ('');}
 
 function big (p)
-{  for (let i = 0;  i < pLen;  i++) {
-   // preload full pics
-     let a = pic [i].split('|');
-     let fn = a[1];
-      parr [i] = new Image ();
-      parr [i].src = path+fn;
-   }
-  let h = '';
+{ let h = '';
   let a = pic [p].split ('|');
   let fn = a[1];
   let cm = a[2];
   let or = screen.orientation.type.substr (0,4);
-  let st = (or == 'land') ? 'height:94vh' : 'width:100vw';
-   h +=    "<center>";
-   h +=    "<img id='big' onclick='full(); un();' style='" + st + "' " +
-                'src="' + path + fn + '"' + ">\n" +
-           "</center>\n";
-   $("#info").html (h);
-   full ();
+  let st = (or == 'land') ? 'height:94vh; ' : 'width:100vw; ';
+   st += 'background-image: url(' + path + fn + ');'
+   h += "<center>\n" +
+        "<div id='big' onclick='full(); un();' style='" + st + "'>\n" +
+        " <p  id='bigtxt'>"+cm+"</p>\n" +
+        "</div>\n" +
+        "</center>\n";
+   $("#full").html (h);
+   full (path + fn);
 }
 
 $(function () {
    navInit ();
    $('a').button ();
-   $('#fold').selectmenu ({ change: reFold, width: 140});
+   $('#year').selectmenu ({ change: reYear, width: 140});
    $('#pset').selectmenu ({ change: rePSet, width: 320});
    $('#prevset').button ().click (prevSet);
    $('#nextset').button ().click (nextSet);
-   redo ();
 });
  </script>
 <? pg_body (); ?>
 
- <span>
-<? select ('fold', $Fold, $fStr);
+<span>
+<? select ('year', $Year, $yStr);
    select ('pset', $PSet, $sStr);
    if ($sPos > 0) { ?>
-  <button id='prevset' title='previous set of pics'>PrevSet</button>
+ <button id='prevset' title='previous set of pics'>PrevSet</button>
 <? }
    if ($sPos+1 < count ($PSet)) { ?>
-  <button id='nextset' title='next set of pics'>NextSet</button>
+ <button id='nextset' title='next set of pics'>NextSet</button>
 <? } ?>
-  &nbsp; &nbsp;
-  <span id='info' style='font-size:16pt'></span>
- </span>
- <div id='redo'></div>
+ &nbsp; &nbsp;
+<? if ($pCom != '')  echo "<span class='comment'>$pCom</span>\n"; ?>
+</span>
+<center>
+<? foreach ($Pic as $i => $p) {
+      $a = explode ('|', $p);   $fn = $a [1];   $cm = $a [2];
+      echo
+" <div onclick='big($i);' class='thumb'>\n" .
+"  <img src='idx/$yStr/$sStr/$fn'>\n" .
+"  <br><span class='thumbtxt'>$cm</span>\n" .
+" </div><div class='thumb'>&nbsp;</div>\n";
+   }
+?>
+ <span id='full'></span>
+</center>
 
 <? pg_foot ();
