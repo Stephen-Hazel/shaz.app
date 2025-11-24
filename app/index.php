9a348238a976359a1e56c08cf47ae41bad5d7d5a
Stephen Hazel <stephen.hazel@gmail.com>
2025-11-24 07:53:04 -0800

    more

--- app/index.php
+++ app/index.php
@@ -7,32 +7,35 @@ require_once "../_inc/app.php";
  <script>
 $(function () {
    init ();
-   jRum ('pc', 10,  0, 0);
-   jRum ('sw',  0, 10, 0);
+   jRum ('a', 10,  0,  0);
+   jRum ('b',  0, 10,  0);
+   jRum ('c',  0,  0, 10);
+   jRum ('d',  5,  0, 10);
+   jRum ('e',  0,  5, 10);
 });
  </script>
 <? pg_body ([
       [$UC['ar-lftup']."home",  "..",  "...take me back hooome"],
    ]); ?>
-<a id='pc' pop class='nav-1'
+<a id='a' pop class='nav-1'
    href='https://pianocheetah.app'>pianocheetah</a><br>
 my baby.<br><br><br>
 
-<a id='sw' pop class='nav-2'
+<a id='b' pop class='nav-2'
    href='https://github.com/Stephen-Hazel/shazware'>shazware</a><br>
 <b>S</b>teve <b>HAZ</b>el's &nbsp; soft<b>WARE</b> -
 some file utilities i wrote for me.<br><br><br>
 
-<a pop class='nav-3'
+<a id='c' pop class='nav-3'
    href='https://github.com/Stephen-Hazel/stv'>stv lib</a><br>
 my common os,ui,midi,snd,syn,etc,etc library (not an app, just source)
 <br><br><br>
 
-<a pop class='nav-4'
+<a id='d' pop class='nav-4'
    href='https://github.com/Stephen-Hazel/shaz.app'>shaz.app</a><br>
 shaz.app website junk (not an app, just html,etc)<br><br><br>
 
-<a pop class='nav-5'
+<a id='e' pop class='nav-5'
    href='https://github.com/Stephen-Hazel/pianocheetah.app'
   >pianocheetah.app</a><br>
 pianocheetah.app website junk (not an app, just html,etc)<br><br><br>
